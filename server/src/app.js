8489d7a1b576bebaec6543cf96be6ff03b67b139
Jorge Ramírez <jramirez@novopayment.com>
2021-01-05 22:10:00 -0400

    Incorpora mecanismo de variables de entorno

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = require('./router')
 const app = express()
-const port = 3000
+const dotenv = require('dotenv')
 
+dotenv.config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(router)
 
+const port = process.env.PORT
 app.listen(port, () => console.log(`Running at http://localhost:${port}`))
