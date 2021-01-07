cb8aabf72d7e9eac550d3a0da8a3e7318fc7eebd
Jorge Ramírez <jramirez@novopayment.com>
2021-01-06 22:53:04 -0400

    Agrega conexión con base de datos

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = require('./router')
 const app = express()
 const dotenv = require('dotenv')
+const dbConnectionHandler = require('./dbConnectionHandler')
 
 dotenv.config()
 app.use(express.json())
@@ -9,4 +10,12 @@ app.use(express.urlencoded({ extended: true }))
 app.use(router)
 
 const port = process.env.PORT
-app.listen(port, () => console.log(`Running at http://localhost:${port}`))
+dbConnectionHandler.connect()
+	.then( () => app.listen(port, () => console.log(`Listening on port ${port}`)) )
+	.then( server => server.on('error', error => {
+		console.log(error)
+		dbConnectionHandler
+			.disconnect()
+			.then( () => process.exit(0) )
+	}))
+	.catch( dbConnectionError => console.log(dbConnectionError) )
---
+++ server/src/dbConnectionHandler.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const dbConnectionHandler = {
+	async connect() {
+		mongoose.connection.on('open', () =>	console.log('Connected to MongoDB server'))
+		mongoose.connection.on('close', () =>	console.log('Closing MongoDB connection'))
+		mongoose.connection.on('error', () =>	console.log('MongoDB connection error'))
+    const connectionString = process.env.DB_CONN_STRING
+    const connectionOptions = {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    }
+		await mongoose.connect(connectionString, connectionOptions)
+	},
+	async disconnect() {
+		await mongoose.connection.close()
+	}
+}
+
+module.exports = dbConnectionHandler
