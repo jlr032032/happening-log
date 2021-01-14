cdadf26299ebc75950c112a53c91d64c581a3b7a
Jorge Ramírez <jramirez@novopayment.com>
2021-01-14 19:37:23 -0400

    Establece paleta de colores de tema  claro

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -3,4 +3,18 @@ import Vuetify from 'vuetify/lib/framework'
 
 Vue.use(Vuetify)
 
-export default new Vuetify({})
+export default new Vuetify({
+	theme: {
+		themes: {
+			light: {
+				primary: '#ffc107',
+				secondary: '#009688',
+				accent: '#4caf50',
+				error: '#f44336',
+				warning: '#ff9800',
+				info: '#2196f3',
+				success: '#8bc34a'
+			}
+		}
+	}
+})
