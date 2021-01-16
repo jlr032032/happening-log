d3a0e03d8b25f37788102ea20f72ff3d98fd2cab
Jorge Ramírez <jramirez@novopayment.com>
2021-01-15 21:25:21 -0400

    Agrega soporte para múltiples layouts

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,40 @@
 <template>
 	<v-app>
-		<v-main>
-			<router-view />
-		</v-main>
+		<template v-if="layout=='layout-0'">
+			<v-main>
+				<router-view />
+			</v-main>
+		</template>
+		<template v-else-if="layout=='layout-1'">
+			<v-app-bar
+				app
+				dense
+				flat
+			>
+				<v-toolbar-title>Registro de Actividades</v-toolbar-title>
+				<template v-slot:extension>
+					<v-tabs grow>
+					</v-tabs>
+				</template>
+			</v-app-bar>
+			<v-main>
+				<router-view />
+			</v-main>
+		</template>
 	</v-app>
 </template>
 
 <script>
 	export default {
-		name: 'App'
+		name: 'App',
+		computed: {
+			layout() {
+				switch(this.$route.path) {
+					case '/':
+						return 'layout-0'
+				}
+				return 'layout-0'
+			}
+		}
 	}
 </script>
